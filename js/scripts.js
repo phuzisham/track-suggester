bbdb52400285f8ad44e5ebf378378a955e5fd92b
David Hale <david.hale7@gmail.com>
2017-08-14 20:05:06 -0700

    refactor code and add padding to course pages

--- js/scripts.js
+++ js/scripts.js
@@ -2,44 +2,31 @@ var ruby = 0;
 var php = 0;
 var css = 0;
 
+function assignVal(question, num) {
+  if (question == 'rb') {
+    ruby += num;
+  } else if (question == 'ph') {
+    php += num;
+  } else if (question == 'cs') {
+    css += num;
+  }
+}
+
 $(document).ready(function() {
   $("form#survey").submit(function(event) {
     event.preventDefault();
-    // vars used for all values
+
     var side = $("#devSide").val();
-    var one = $("input:radio[name=optradio1]:checked").val();
-    var two = $("input:radio[name=optradio2]:checked").val();
-    var three = $("input:radio[name=optradio3]:checked").val();
-    var four = $("input:radio[name=optradio4]:checked").val();
-    var five = $("input:radio[name=optradio5]:checked").val();
-    var six = $("input:radio[name=optradio6]:checked").val();
+    var radioQ = ['optradio1', 'optradio2', 'optradio3', 'optradio4', 'optradio5', 'optradio6'];
 
-    function assignVal(question) {
-      if (question == 'rb') {
-        ruby += 1;
-      } else if (question == 'ph') {
-        php += 1;
-      } else if (question == 'cs') {
-        css += 1;
-      }
-    }
-
-    if (one==undefined || two==undefined || three==undefined) {
-      alert("Please select a response for each question");
-      return;
-    } if (side == 'rb') {
-      ruby += 2;
-    } else if (side == 'ph') {
-      php += 2;
-    } else if (side == 'cs') {
-      css += 2;
-    }
-    assignVal(one);
-    assignVal(two);
-    assignVal(three);
-    assignVal(four);
-    assignVal(five);
-    assignVal(six);
+    assignVal(side, 2);
+    radioQ.forEach(function(radio) {
+      var userInput= $("input:radio[name=" + radio + "]:checked").val();
+      if (userInput==undefined) {
+        alert("Please select a response for each question");
+        return;
+      }  assignVal(userInput, 1);
+    });
 
     // if statements to show track with highest score
     if (ruby > php && ruby > css) {
